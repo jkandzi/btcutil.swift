4295966332b2dab829974b96964bc24c8ecf76fd
Justus Kandzi <justus.kandzi@gmail.com>
2018-02-27 16:28:07 +0100

    Initial Commit

---
+++ btcutil/btcutil.h
@@ -0,0 +1,19 @@
+//
+//  btcutil.h
+//  btcutil
+//
+//  Created by Justus Kandzi on 27.02.18.
+//  Copyright © 2018 Justus Kandzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for btcutil.
+FOUNDATION_EXPORT double btcutilVersionNumber;
+
+//! Project version string for btcutil.
+FOUNDATION_EXPORT const unsigned char btcutilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <btcutil/PublicHeader.h>
+
+
